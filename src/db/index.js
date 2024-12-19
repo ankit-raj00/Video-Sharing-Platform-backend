5febba40d243af24532ef6d8f543f08039a1fc98
ankit-raj00 <ankitkumar5259211@gmail.com>
2024-12-19 18:33:16 +0530

    important lesson

--- src/db/index.js
+++ src/db/index.js
@@ -20,3 +20,7 @@ export default connectDB
 
 
 /* chatgpt- "https://chatgpt.com/share/676410ff-5544-800f-833a-09da88c3ee11" */
+/*Import lesson - 
+1) Use try and catch
+2)process.exit bcz if db connection fail stop all code execution
+3)constant.js istead of constent same in index file ...db/index.js */
