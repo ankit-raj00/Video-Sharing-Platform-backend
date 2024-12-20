096573097f7300cd503f839d759d7f0456062794
ankit-raj00 <ankitkumar5259211@gmail.com>
2024-12-20 17:47:46 +0530

    util code

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import cors from "cors"
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}))
+
+app.use(express.json({limit : "16kb"}))
+app.use(express.urlencoded({extended : true , limit : "16kb"}))
+app.use(express.cookieParser())
+
+export {app}
+
+
+
+
+
+
+
+
+
+
+//whenever we use middleware or we have to do config setting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import connectDB from "./db/index.js";
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is running at port : ${process.env.Port}`);
+    })
+})
+.catch((err)=>{
+    console.log('Mongo DB connection failed !!' . err);
+
+})
 
 
 
